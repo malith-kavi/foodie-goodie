18674f206fabe85aff75c597c5bdc06432483a2e
Malith Kavinda <malithkavi30@gmail.com>
2024-03-20 02:06:42 +0530

    03.20

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -106,48 +106,99 @@ class RootPage extends StatelessWidget {
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                'Drawer Header',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
+            DrawerHeader(
+              child: Center(
+                child: Row(
+                  children: [
+                    IconButton(
+                        icon: Image.asset('assets/images/Male.png',
+                            width: 75, height: 75),
+                        onPressed: () {}),
+                    const Text(
+                      'User Name',
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 0, 0, 0),
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ],
                 ),
               ),
             ),
             ListTile(
-              leading: const Icon(Icons.message),
-              title: const Text('message'),
+              title: const Text('Check For Updates'),
+              leading: const Icon(Icons.system_update_alt_rounded),
               onTap: () {},
             ),
             ListTile(
-              leading: const Icon(Icons.person),
-              title: const Text('person'),
+              leading: const Icon(Icons.settings),
+              title: const Text('Settings'),
               onTap: () {},
             ),
             ListTile(
-              leading: const Icon(Icons.person),
-              title: const Text('person'),
+              leading: const Icon(Icons.call),
+              title: const Text('Contact Us'),
               onTap: () {},
             ),
             ListTile(
-              leading: const Icon(Icons.person),
-              title: const Text('person'),
+              leading: const Icon(Icons.info_outline_rounded),
+              title: const Text('About Us'),
               onTap: () {},
             ),
             ListTile(
-              leading: const Icon(Icons.person),
-              title: const Text('person'),
+              leading: const Icon(Icons.logout),
+              title: const Text('Log Out'),
               onTap: () {},
             ),
           ],
         ),
       ),
       body: Center(
-        child: Text('Home Page Content'),
+        child: (ListView(
+          padding: const EdgeInsets.all(20),
+          children: <Widget>[
+            Container(
+              height: 100,
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black12,
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3)),
+                ],
+              ),
+              child: Row(
+                children: [
+                  ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset(
+                        'assets/images/picture1.png',
+                      ))
+                ],
+              ),
+
+              //child: const Center(child: Text('Entry A')),
+            ),
+            // Container(
+            //   height: 50,
+            //   color: Colors.black38,
+            //   child: const Center(child: Text('Entry B')),
+            // ),
+            // Container(
+            //   height: 50,
+            //   color: Colors.black38,
+            //   child: const Center(child: Text('Entry C')),
+            // ),
+            // Container(
+            //   height: 50,
+            //   color: Colors.black38,
+            //   child: const Center(child: Text('Entry D')),
+            // ),
+          ],
+        )),
       ),
     );
   }
